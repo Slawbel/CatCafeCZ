658d3700d9575b6b8d7296d892e2915b805d0477
Viacheslav Belov <vektorification@gmail.com>
2024-02-02 11:49:47 +0100

    Cell has nil value. Should be solved

--- CatCafeCZ/ViewController.swift
+++ CatCafeCZ/ViewController.swift
@@ -4,16 +4,40 @@
 //
 //  Created by Viacheslav Belov on 01.02.2024.
 //
-
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    private var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellTableViewControllerForViewController")
+        
+        
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.top.leading.bottom.trailing.equalTo(view)
+        }
+        
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellTableViewControllerForViewController") as? CellTableViewControllerForViewController
+        cell?.setup(text: "cell")
+        cell?.backgroundColor = .white
+        return cell!
+    }
 
 }
 
