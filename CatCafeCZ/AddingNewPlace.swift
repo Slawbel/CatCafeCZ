d1589b522fb81d768fc48e40ee67ee581e576839
Viacheslav Belov <vektorification@gmail.com>
2024-02-13 03:53:17 +0100

    fixed scrolling and dismissing of keyboard

--- CatCafeCZ/AddingNewPlace.swift
+++ CatCafeCZ/AddingNewPlace.swift
@@ -11,19 +11,31 @@ class AddingNewPlace: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveTapped))
         
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(CellForAddingNewPlace.self, forCellReuseIdentifier: "CellForAddingNewPlace")
-        tableView.allowsSelection = false
-        tableView.isScrollEnabled = false
+        tableView.allowsSelection = true
+        tableView.isScrollEnabled = true
         tableView.backgroundColor = .black
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         
         view.addSubview(tableView)
+        view.addGestureRecognizer(tapGesture)
         
         tableView.snp.makeConstraints { make in
             make.leading.bottom.trailing.equalTo(view)
             make.top.equalTo(view)
         }
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     @objc private func cancelTapped() {
@@ -42,6 +54,11 @@ class AddingNewPlace: UIViewController, UITableViewDelegate, UITableViewDataSour
         return 1
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        print("hello")
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellForAddingNewPlace", for: indexPath) as? CellForAddingNewPlace
         cell?.layer.cornerRadius = 20
@@ -55,7 +72,6 @@ class AddingNewPlace: UIViewController, UITableViewDelegate, UITableViewDataSour
         default: break
         }
         
-        
         return cell!
     }
     
@@ -67,4 +83,26 @@ class AddingNewPlace: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+                let keyboardFrameValue = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.size.height, right: 0)
+        tableView.contentInset = contentInsets
+        tableView.scrollIndicatorInsets = contentInsets
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        tableView.contentInset = .zero
+        tableView.scrollIndicatorInsets = .zero
+    }
 }
+
+
--- CatCafeCZ/CellForAddingNewPlace.swift
+++ CatCafeCZ/CellForAddingNewPlace.swift
@@ -20,9 +20,11 @@ class CellForAddingNewPlace: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+                
         addNameStackView.layer.cornerRadius = 20
         addNameStackView.axis = .vertical
+        
+        self.selectionStyle = .none
         
         cellImage.contentMode = .center
 
@@ -32,11 +34,8 @@ class CellForAddingNewPlace: UITableViewCell {
         cellImage.snp.makeConstraints{ make in
             make.leading.trailing.top.bottom.equalToSuperview()
         }
-        
-        
-        
-        
     }
+    
     
     func setupImage (text: String?) {
         cellImage.image = UIImage(named: text!)
@@ -46,11 +45,17 @@ class CellForAddingNewPlace: UITableViewCell {
         self.addNameStackView.layer.cornerRadius = 20
         self.addNameStackView.axis = .vertical
         
-        self.addNameLabel.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        self.addNameLabel.font = UIFont.systemFont(ofSize: 19, weight: .thin)
+        self.addNameLabel.text = "Name"
  
-        self.addNameTF.attributedPlaceholder = NSAttributedString(string: " Enter name of place here", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        self.addNameTF.attributedPlaceholder = NSAttributedString(string: " Enter name of place here", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)])
         self.addNameTF.backgroundColor = .black
         self.addNameTF.layer.cornerRadius = 10
+        self.addNameTF.autocapitalizationType = .sentences
+        self.addNameTF.returnKeyType = .done
+        self.addNameTF.textColor = .white
+        self.addNameTF.delegate = self
+
         
         contentView.addSubview(addNameStackView)
         addNameStackView.addSubview(addNameLabel)
@@ -67,7 +72,7 @@ class CellForAddingNewPlace: UITableViewCell {
         }
         
         addNameTF.snp.makeConstraints{ make in
-            make.top.equalTo(addNameLabel.snp.bottom).offset(5)
+            make.top.equalTo(addNameLabel.snp.bottom).offset(7)
             make.leading.trailing.bottom.equalToSuperview().inset(10)
         }
     }
@@ -76,11 +81,16 @@ class CellForAddingNewPlace: UITableViewCell {
         self.addLocationStackView.layer.cornerRadius = 20
         self.addLocationStackView.axis = .vertical
         
-        self.addLocationLabel.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        self.addLocationLabel.font = UIFont.systemFont(ofSize: 19, weight: .thin)
+        self.addLocationLabel.text = "Location"
  
-        self.addLocationTF.attributedPlaceholder = NSAttributedString(string: " Enter location of place here", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        self.addLocationTF.attributedPlaceholder = NSAttributedString(string: " Enter location of place here", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)])
         self.addLocationTF.backgroundColor = .black
         self.addLocationTF.layer.cornerRadius = 10
+        self.addLocationTF.autocapitalizationType = .sentences
+        self.addLocationTF.returnKeyType = .done
+        self.addLocationTF.textColor = .white
+        self.addLocationTF.delegate = self
         
         contentView.addSubview(addLocationStackView)
         addLocationStackView.addSubview(addLocationLabel)
@@ -97,7 +107,7 @@ class CellForAddingNewPlace: UITableViewCell {
         }
         
         addLocationTF.snp.makeConstraints{ make in
-            make.top.equalTo(addLocationLabel.snp.bottom).offset(5)
+            make.top.equalTo(addLocationLabel.snp.bottom).offset(7)
             make.leading.trailing.bottom.equalToSuperview().inset(10)
         }
     }
@@ -106,11 +116,16 @@ class CellForAddingNewPlace: UITableViewCell {
         self.addTypeStackView.layer.cornerRadius = 20
         self.addTypeStackView.axis = .vertical
         
-        self.addTypeLabel.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        self.addTypeLabel.font = UIFont.systemFont(ofSize: 19, weight: .thin)
+        self.addTypeLabel.text = "Type"
  
-        self.addTypeTF.attributedPlaceholder = NSAttributedString(string: " Enter type of place here", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        self.addTypeTF.attributedPlaceholder = NSAttributedString(string: " Enter type of place here", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)])
         self.addTypeTF.backgroundColor = .black
         self.addTypeTF.layer.cornerRadius = 10
+        self.addTypeTF.autocapitalizationType = .sentences
+        self.addTypeTF.returnKeyType = .done
+        self.addTypeTF.textColor = .white
+        self.addTypeTF.delegate = self
         
         contentView.addSubview(addTypeStackView)
         addTypeStackView.addSubview(addTypeLabel)
@@ -127,12 +142,19 @@ class CellForAddingNewPlace: UITableViewCell {
         }
         
         addTypeTF.snp.makeConstraints{ make in
-            make.top.equalTo(addTypeLabel.snp.bottom).offset(5)
+            make.top.equalTo(addTypeLabel.snp.bottom).offset(7)
             make.leading.trailing.bottom.equalToSuperview().inset(10)
         }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CellForAddingNewPlace: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
